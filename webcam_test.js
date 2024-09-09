d336291febc6765c335a81ead36a8b464e8c4744
balarajagopal <bala.cis@gmail.com>
2024-09-09 13:49:19 -0400

    Update webcam_test.js

--- webcam_test.js
+++ webcam_test.js
@@ -66,7 +66,7 @@ function populateCameraList() {
 
 		if (document.querySelector('#camera-select').options.length > 1){
 			document.querySelector('#camera-select').selectedIndex = 1;
-			document.querySelector('#camera-select').clientLeft;
+			document.querySelector('#camera-select').dispatchEvent(new Event('change'));
 		}
 	});
 }
